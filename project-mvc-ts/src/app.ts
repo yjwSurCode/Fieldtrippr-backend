405d908b8c1799b3c1ccd235e8be68327aafc6d0
jia <1424800753@qq.com>
2022-08-11 17:48:30 +0800

    feat:CI/CD以及socket.io 第一步

--- project-mvc-ts/src/app.ts
+++ project-mvc-ts/src/app.ts
@@ -3,6 +3,10 @@ import * as path from "path";
 import * as bodyParser from "body-parser";
 import express from "express";
 import router from "./route/index";
+import { Server } from "socket.io";
+
+const server = require("http").createServer();
+const io = new Server(server);
 
 const { expressjwt: jwt } = require("express-jwt");
 /** 与生成token的密钥要一致! */
@@ -22,18 +26,38 @@ app.use((req: any, res: any, next: any) => {
 });
 
 /** 使用expressJWT 验证token是否过期 */
-app.use(
-  jwt({
-    secret: SECRET_KEY,
-    algorithms: ["HS256"],
-  }).unless({
-    /** 指定路径不经过Token解析 */
-    path: ["/register", "/login"],
-  })
-);
+// app.use(
+//   jwt({
+//     secret: SECRET_KEY,
+//     algorithms: ["HS256"],
+//   }).unless({
+//     /** 指定路径不经过Token解析 */
+//     path: ["/register", "/login", "/"],
+//   })
+// );
 
-/* 路由 */
-app.use("/", router);
+/** 路由 */
+// app.use("/", router);
+
+/** 静态文件夹 */
+// app.set("public", __dirname + "/public");
+
+app.use(express.static("public"));
+
+app.get("/index", function (req, res) {
+  res.sendFile(path.join(__dirname, "../public", "index.html"));
+});
+
+// app.use("/static", express.static("public"));
+// console.log(express.static(__dirname + "/"));
+
+io.on("connection", (socket) => {
+  socket.emit("success", "连接到服务器");
+
+  socket.on("disconnect", () => {
+    io.emit("quit", socket.id);
+  });
+});
 
 app.listen(3000, () => {
   console.log(`App runing at http://localhost:3000`);
